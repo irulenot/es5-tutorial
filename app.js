0dc146773aa3a5e6a65df98495de6dc6267b462e
irulenot <anthony.i.bilic@gmail.com>
2017-09-19 22:30:22 -0700

    Made API call and are going to tackle how to handle it on next commit

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 "use strict";
 
-// init.js availiable as called before app.js, thus can access searchButton
 searchButton.addEventListener('click', searchWeather);
 
-// searchWeatheris hoisted up by JS
 function searchWeather() {
-  console.log(searchCity.value);
+  var cityName = searchCity.value;
+  // use trim to account for blank spaces
+  if(cityName.trim().length == 0){
+    return alert('Please enter a City Name');
+  }
+  var http = new XMLHttpRequest();
+  var apiKey = '426eee677455fbc5010c84a10a0007c4'
+  // url key, must add http:// to start
+  var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName;
+  var method = 'GET';
+
+  http.open(method, url);
+  http.onreadystatechange = function() {
+    if(http.readyState == XMLHttpRequest.DONE && http.status == 200){
+      // handles what will be done if request works
+    }else if (http.readyState === XMLHttpRequest.DONE) {
+      alert('Something went wrong!');
+    }
+  };
+
 }
