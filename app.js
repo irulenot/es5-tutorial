f090b40b9028a44c62651015d38f924ca28a6e8b
irulenot <anthony.i.bilic@gmail.com>
2017-09-19 22:39:47 -0700

    Created Weather object to handle data from API call

--- app.js
+++ app.js
@@ -4,20 +4,19 @@ searchButton.addEventListener('click', searchWeather);
 
 function searchWeather() {
   var cityName = searchCity.value;
-  // use trim to account for blank spaces
   if(cityName.trim().length == 0){
     return alert('Please enter a City Name');
   }
+
   var http = new XMLHttpRequest();
   var apiKey = '426eee677455fbc5010c84a10a0007c4'
-  // url key, must add http:// to start
   var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName;
   var method = 'GET';
-
   http.open(method, url);
   http.onreadystatechange = function() {
     if(http.readyState == XMLHttpRequest.DONE && http.status == 200){
-      // handles what will be done if request works
+      // API tells us we will get JSON string back
+      var data = JSON.parse(http.responseText);
     }else if (http.readyState === XMLHttpRequest.DONE) {
       alert('Something went wrong!');
     }
